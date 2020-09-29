53b33c820e5284a4025b6f0db1304640ef62aa71
data-hunter <gabrielinsmelo@gmail.com>
2020-09-29 13:45:25 -0300

    Added an extra flag in a compiling tip

--- project/src/extras.cpp
+++ project/src/extras.cpp
@@ -5,5 +5,5 @@
 /*
     Compiling tips:
 
-        - When working in a small project, where you can compile all source files in small time, it is usefull to use the star operator for compiling. E.g.: $ g++ *.cpp -o main
+        - When working in a small project, where you can compile all source files in small time, it is usefull to use the star operator for compiling. E.g.: $ g++ *.cpp -std=c++11 -o main
 */
