8a1ff552511f172f300fb259b1fc63265d9784bf
data-hunter <gabrielinsmelo@gmail.com>
2020-09-29 11:53:46 -0300

    Added extras file with compiling tips

---
+++ project/src/extras.cpp
@@ -0,0 +1,9 @@
+/*
+    This file contains some extra C++ resources, good practices and compiling tips.
+*/
+
+/*
+    Compiling tips:
+
+        - When working in a small project, where you can compile all source files in small time, it is usefull to use the star operator for compiling. E.g.: $ g++ *.cpp -o main
+*/
