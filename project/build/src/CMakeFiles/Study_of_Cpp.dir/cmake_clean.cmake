5662e0234787be196a53d31d2bd004f0c0e63a15
data-hunter <GabrielLins64>
2021-01-26 17:32:00 -0300

    Added CMakeLists.txt to main project

---
+++ project/build/src/CMakeFiles/Study_of_Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Study_of_Cpp.dir/concepts.cpp.o"
+  "CMakeFiles/Study_of_Cpp.dir/statements.cpp.o"
+  "CMakeFiles/Study_of_Cpp.dir/main.cpp.o"
+  "CMakeFiles/Study_of_Cpp.dir/data_types.cpp.o"
+  "../../bin/Study_of_Cpp.pdb"
+  "../../bin/Study_of_Cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Study_of_Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
